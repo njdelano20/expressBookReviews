ea1633db637944bc5a8d87b2e60b30bcdae83594
Nick Delano <njdelano20@gmail.com>
2025-05-26 13:45:34 -0400

    Completed tasks 2-5: Implemented get requests on general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,26 +17,67 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+
+  const book = books[ISBN];
+
+  if (book) {
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  }
+  else {
+    return res.status(404).json({message: "Book not found"});
+  }
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  const booksArray = Object.values(books);
+
+  const foundBook = booksArray.filter(book => book.author === author);
+
+  if (foundBook) {
+    return res.status(200).send(JSON.stringify(foundBook, null, 4));
+  }
+  else {
+    return res.status(404).json({message: "Author not found in our library"})
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  const booksArray = Object.values(books);
+
+  const foundBook = booksArray.filter(book => book.title === title);
+
+  if (foundBook) {
+    return res.status(200).send(JSON.stringify(foundBook, null, 4));
+  } else {
+  return res.status(404).json({message: "Title not found in our library"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const ISBN = req.params.isbn;
+
+    const book = books[ISBN];
+
+    if (!book) {
+        return res.status(404).json({message: "Book not found in our library"});
+    }
+
+    const reviews = book[reviews];
+
+    if (reviews) {
+        return res.status(200).send(JSON.stringify(reviews, null, 4));
+    } else {
+    return res.status(404).json({message: `There are no reviews for ${book.title}`});
+    }
+  });
 
 module.exports.general = public_users;
